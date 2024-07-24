7a0ae6e984fd7c09c1893d814ea248dde0189b67
a.onishchuk <a.onishchuk@itgroup42.ru>
2024-07-24 08:14:54 +0300

    it works

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
 const process = require('process');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 require('dotenv').config()
 const TOKEN = process.env.BOT_TOKEN;
 const server = express();
 const bot = new TelegramBot(TOKEN, {
     polling: true
 });
-const port = process.env.PORT || 7000;
-const gameName = "FlappyAgain";
+const port = process.env.PORT || 7005;
+const gameName = process.env.GAME_NAME;
 const queries = {};
-server.use(express.static(path.join(__dirname, 'flappy_test')));
+// server.use(express.static(path.join(__dirname, 'flappy_test')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
@@ -21,7 +21,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://mstrhw.github.io/flappy_test/";
+        let gameurl = process.env.APP_ENDPOINT;
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
@@ -35,21 +35,5 @@ bot.on("inline_query", function (iq) {
         game_short_name: gameName
     }]);
 });
-server.get("/highscore/:score", function (req, res, next) {
-    if (!Object.hasOwnProperty.call(queries, req.query.id)) return next();
-    let query = queries[req.query.id];
-    let options;
-    if (query.message) {
-        options = {
-            chat_id: query.message.chat.id,
-            message_id: query.message.message_id
-        };
-    } else {
-        options = {
-            inline_message_id: query.inline_message_id
-        };
-    }
-    bot.setGameScore(query.from.id, parseInt(req.params.score), options,
-        function (err, result) {});
-});
+
 server.listen(port);
